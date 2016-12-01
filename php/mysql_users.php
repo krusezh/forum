69ebbb235db126d9fd7e0837ee35a898cfd8273c
krusezh <krusezh@gmail.com>
2016-12-01 20:56:36 +0800

    开始写注册页面

---
+++ php/mysql_users.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dim
+ * Date: 2016/12/1
+ * Time: 下午8:36
+ */
+
+$register = 'registeruser';
+$registerpwd = '1234567890,.?';
+
---
+++ php/register.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dim
+ * Date: 2016/12/1
+ * Time: 下午8:10
+ */
+
+include_once("mysql_users.php");
+
+$user_name = $_POST['username'];
+$user_password = $_POST['password'];
+
