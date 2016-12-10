85742b7bfbf82db4dfcf22f93d4ffa14ff2ea47a
SlaneYang <904509221@qq.com>
2016-12-10 19:34:49 +0800

    样式表

--- php/display_html.php
+++ php/display_html.php
@@ -13,6 +13,8 @@ function display_head($title) {
     <head>
         <meta charset="UTF-8">
         <title><?php echo $title;?></title>
+        <link rel="stylesheet" href="./../static/styles/reset.css">
+        <link rel="stylesheet" href="./../static/styles/main.css">
     </head>
     <body>
     </body>
@@ -22,25 +24,27 @@ function display_head($title) {
 
 function display_top($place='') {
 ?>
-    <div>
-        <span><a href="http://localhost/phpstorm/forum/">首页</a></span>
-        <?php
-        if(check_valid_user()) {
-            $username = $_SESSION['valid_user'];
-            ?>
-            <span><a href="<?php echo $place;?>profile.php?username=<?php echo $username;?>"><?php echo $username;?></a></span>
-            <span><a href="<?php echo $place;?>write_article.php">创建新主题</a></span>
-            <span><a href="<?php echo $place;?>logout.php">登出</a></span>
+    <div class="header">
+        <div>
+            <span><a href="http://localhost/phpstorm/forum/">首页</a></span>
             <?php
-        }
-        else {
+            if(check_valid_user()) {
+                $username = $_SESSION['valid_user'];
+                ?>
+                <span><a href="<?php echo $place;?>profile.php?username=<?php echo $username;?>"><?php echo $username;?></a></span>
+                <span><a href="<?php echo $place;?>write_article.php">创建新主题</a></span>
+                <span><a href="<?php echo $place;?>logout.php">登出</a></span>
+                <?php
+            }
+            else {
+                ?>
+                <span><a href="<?php echo $place;?>register.php">注册</a></span>
+                <span><a href="<?php echo $place;?>login.php">登录</a></span>
+                <?php
+            }
             ?>
-            <span><a href="<?php echo $place;?>register.php">注册</a></span>
-            <span><a href="<?php echo $place;?>login.php">登录</a></span>
-            <?php
-        }
-        ?>
-    </div>
+        </div>
+    </div>    
 <?php
 }
 
