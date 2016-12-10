c743244d2c9470dc3186d94844ab51bb22548542
SlaneYang <904509221@qq.com>
2016-12-10 19:57:17 +0800

    S

--- php/display_html.php
+++ php/display_html.php
@@ -24,26 +24,28 @@ function display_head($title) {
 
 function display_top($place='') {
 ?>
-    <div class="header wrapper clearfix">
+    <div class="header">
+        <div class="wrapper clearfix">
         <div class="header-logo">FORUM</div>
-        <div class="header-nav">
-            <span><a href="http://localhost/phpstorm/forum/">首页</a></span>
-            <?php
-            if(check_valid_user()) {
-                $username = $_SESSION['valid_user'];
-                ?>
-                <span><a href="<?php echo $place;?>profile.php?username=<?php echo $username;?>"><?php echo $username;?></a></span>
-                <span><a href="<?php echo $place;?>write_article.php">创建新主题</a></span>
-                <span><a href="<?php echo $place;?>logout.php">登出</a></span>
+            <div class="header-nav">
+                <span><a href="http://localhost/phpstorm/forum/">首页</a></span>
                 <?php
-            }
-            else {
+                if(check_valid_user()) {
+                    $username = $_SESSION['valid_user'];
+                    ?>
+                    <span><a href="<?php echo $place;?>profile.php?username=<?php echo $username;?>"><?php echo $username;?></a></span>
+                    <span><a href="<?php echo $place;?>write_article.php">创建新主题</a></span>
+                    <span><a href="<?php echo $place;?>logout.php">登出</a></span>
+                    <?php
+                }
+                else {
+                    ?>
+                    <span><a href="<?php echo $place;?>register.php">注册</a></span>
+                    <span><a href="<?php echo $place;?>login.php">登录</a></span>
+                    <?php
+                }
                 ?>
-                <span><a href="<?php echo $place;?>register.php">注册</a></span>
-                <span><a href="<?php echo $place;?>login.php">登录</a></span>
-                <?php
-            }
-            ?>
+            </div>
         </div>
     </div>    
 <?php
